076129a0b9b7b5f7172151c641accb37db92582b
Renan <renan_ftb@hotmail.com>
2023-10-14 00:38:06 -0300

    implementacao de autenticacao

---
+++ src/main/java/br/com/ifsp/boletim/configuration/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package br.com.ifsp.boletim.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+        	.authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .permitAll()
+            .successForwardUrl("/index")
+            .and()
+            .logout()
+            .permitAll()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/login")
+            .and()
+            .csrf()
+            .disable();
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth, PasswordEncoder passwordEncoder) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder.encode("password"))
+            .roles("USER")
+            .and()
+            .withUser("admin")
+            .password(passwordEncoder.encode("admin"))
+            .roles("ADMIN");
+    }
+
+}
--- src/main/java/br/com/ifsp/boletim/controller/LoginController.java
+++ src/main/java/br/com/ifsp/boletim/controller/LoginController.java
@@ -1,31 +1,19 @@
 package br.com.ifsp.boletim.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.ifsp.boletim.model.LoginModel;
-import br.com.ifsp.boletim.service.LoginService;
-
 @Controller
-@RequestMapping("login")
 public class LoginController {
 	
-	@Autowired
-	private LoginService loginService;
-	
-	@GetMapping
-	public String login() {
-		return "login";
-	}
-	
-	@PostMapping
-	public String login(@Validated LoginModel login) {
-		return loginService.efetuarLogin(login);
-	}
-	
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
 
+    @RequestMapping({ "/index", "/" })
+    public String index() {
+        return "index";
+    }
+	
 }
